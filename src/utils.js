13ad41c4561a1050fc588d4209befe05198df4c8
Overwatch-pr7 <sohampr7@gmail.com>
2026-08-11 13:00:33 +0530

    Textbox generation and dialogue display

---
+++ src/utils.js
@@ -0,0 +1,32 @@
+//dialogue display
+export function displayDialogue(text, onDisplayEnd){
+    const dialogueUI=document.getElementById("textbox-container");
+    const dialogue=document.getElementById("dialogue");
+
+    dialogueUI.style.display="block";
+
+    //text scrolling logic
+    let index=0;  
+    let currentText="";
+    const intervalRef= setInterval(()=>{ //this runs every 5 secs
+            if(index<text.length){
+                currentText+=text[index];
+                dialogue.innerHTML=currentText; //beware of cross-site-scripting when u take user input via innerHTML
+                index++;
+                return;
+            }
+            clearInterval(intervalRef);
+    }, 5);
+
+    //close button functionality
+    const closeButton=document.getElementById("close")
+
+    function onCloseButtonClick(){
+        onDisplayEnd();
+        dialogueUI.style.display= "none";
+        dialogue.innerHTML="";
+        clearInterval(intervalRef);
+        closeButton.removeEventListener("click", onCloseButtonClick);//recursive. remove function wihin itself
+    }
+    closeButton.addEventListener("click", onCloseButtonClick);
+}
